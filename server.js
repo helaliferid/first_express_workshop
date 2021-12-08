19f7cf11678840a6f1a66d78b3ccbbc90cb88fdc
Ferid HELALI <92591821+helaliferid@users.noreply.github.com>
2021-12-08 12:16:32 +0100

    todo todo crud

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const port=3030;
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json());
+app.use(express.json())
 
 const todos=[
     {id:1,task:"read mails"},
     {id:3,task:"verify invoices"},
     {id:4,task:"call my kids"},
 ]
+
+// use fs to persiste / retrive todos
 
 app.get('/todo',function(req,res){
     res.json(todos);
@@ -24,6 +26,17 @@ app.post('/todo',function(req,res){
     let todo=req.body;
     console.log(todo);
     todos.push(todo);
+    res.json(todos);
+})
+
+
+// to copmlete update todo
+app.put('/todo/:id',function(req,res){
+    res.json(todos);
+})
+
+//to complete delete todo
+app.delete('/todo/:id',function(req,res){
     res.json(todos);
 })
 
